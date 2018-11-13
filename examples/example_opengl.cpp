944063fa2985065f4620a99cb58da055e070ea17
Michael Novén <michael.f.noven@gmail.com>
2018-11-13 16:21:13 +0100

    Add opengl example

---
+++ examples/example_opengl.cpp
@@ -0,0 +1,14 @@
+#include "direwolf/renderengine.h"
+
+#include <iostream>
+
+int main() {
+    // Setup renderer
+    // dwf::InitData initData { dwf::RendererType::RASTERIZER, dwf::BackendType::VULKAN };
+    // dwf::PlatformData platformData = { nullptr };
+    // auto engine = std::make_unique<dwf::RenderEngine>(platformData, initData);
+
+    std::cout << "\n\nPress enter to EXIT";
+    std::cin.get();
+    return 0;
+}
