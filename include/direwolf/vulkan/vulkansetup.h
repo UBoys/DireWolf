2158bceff0cc50cdec9e752802efac4e575bec32
Oskar Carlbaum <oskarcarlbaum@gmail.com>
2018-11-15 23:05:17 +0100

    REFACTOR: vulkansetup.h can now be used as a public interface (#2)

--- include/direwolf/vulkan/vulkansetup.h
+++ include/direwolf/vulkan/vulkansetup.h
@@ -1,41 +1,66 @@
+/* **********************************************************************
+Intended usage:
+
+ 1. Initialize vulkan
+        InitializeVulkan()      [will open runtime libraries and load global level functions]
+
+ 2. Create vulkan instance object with preferences
+    a)  [optional] See which instance extensions that are available by calling
+            GetAvailableInstanceExtensions()
+
+    b)  [optional] Create a vector<const char*> with the names of desired instance extensions
+            Ensure these desired extensions exists in the vector returned by
+            GetAvailableInstanceExtensions by calling IsExtensionSupported()
+
+    c)  Create a VulkanInstanceData object and modify it as you please (Include data from 2a and 2b if you please)
+
+    d)  Create the vulkan instance using the VulkanInstanceData you setup in step 2c) by calling
+        CreateVulkanInstance(VulkanInstanceData)
+
+ 5. Choose which physical device to use
+    a)  Get available physical devices
+            GetPhysicalDevices()
+
+    b)  See what extensions that are available on each devices
+            GetPhysicalDeviceExtensions()
+
+    c)  TODO: Pick the one best suited and do much more stuff afterwards ;)
+
+    ... TODO:
+
+********************************************************************** */
+
 #pragma once
 
-#include "vulkan/vulkanfunctions.h"
+#if !defined(_WIN32) && !defined(__APPLE__) && !defined(__linux)
+#error "The DireWolf renderer is not yet setup for Vulkan on this platform. Supported operating systems are Linux, Windows and MacOS";
+#endif
 
-#include <stdint.h>
+#include "vulkan/vulkan.h"
+
 #include <vector>
 
 #if defined _WIN32
 #include <windows.h>
 #endif // _WIN32
 
-namespace dw {
-namespace vulkan {
+namespace dw::vulkan {
 
-class TempVulkanSetupObject {
-public:
-    // TODO: add struct to hold initialize data.. e.g. desiredExtensions and names etc. See createVulkanInstance implementation to get an idea of what it should hold
-    TempVulkanSetupObject(std::vector<const char*>* desiredExtensions = nullptr);
-    bool initialize(std::vector<const char*>* desiredExtensions);
-    bool isValid() const { return m_isValid; }
-    static std::vector<const char*> getDefaultInstanceExtensions();
-
-private: // variables
-    bool m_isValid;
-    VkInstance m_instance;
-
-private: // functions
-    bool initLibs();
-    bool initProcAddr();
-    bool isExtensionSupported(const char* extension, std::vector<VkExtensionProperties>* availableExtensions = nullptr) const;
-    bool loadGlobalLevelFunctions();
-    bool loadInstanceLevelFunctions();
-    bool getPhysicalDevices(std::vector<VkPhysicalDevice>& outAvailableDevices);
-    bool getPhysicalDeviceExtensions(const VkPhysicalDevice& device, std::vector<VkExtensionProperties>& outAvailableExtensions);
-    bool loadInstanceLevelFunctionsFromExtensions(const std::vector<const char*>* enabledExtensions = nullptr);
-    bool createVulkanInstance(std::vector<const char*>* desiredExtensions = nullptr);
-    bool getAvailableInstanceExtensions(std::vector<VkExtensionProperties>& outAvailableExtensions) const;
+struct VulkanInstanceInitData {
+    char* applicationName = "NullNameApplication";
+    char* engineName = "NullNameEngine";
+    std::vector<VkExtensionProperties>* availableExtensionsHandle = nullptr;
+    std::vector<const char*>* desiredExtensions = nullptr;
+    // TODO: Extend this with more options?
 };
 
-} // namespace vulkan
-} // namespace dw
+bool InitializeVulkan();
+VkInstance CreateVulkanInstance(const VulkanInstanceInitData& initData);
+std::vector<VkExtensionProperties> GetAvailableInstanceExtensions();
+std::vector<VkPhysicalDevice> GetPhysicalDevices(const VkInstance& instance);
+std::vector<VkExtensionProperties> GetPhysicalDeviceExtensions(const VkPhysicalDevice& device);
+VkPhysicalDeviceFeatures GetPhysicalDeviceFeatures(const VkPhysicalDevice& device);
+VkPhysicalDeviceProperties GetPhysicalDeviceProperties(const VkPhysicalDevice& device);
+bool IsExtensionSupported(const char* extension, const std::vector<VkExtensionProperties>& availableExtensions);
+
+} // namespace dw::vulkan
--- src/vulkan/vulkanutils.cpp
+++ src/vulkan/vulkanutils.cpp
@@ -3,8 +3,139 @@
 #include <iomanip>
 #include <sstream>
 
-// TODO: fix namespaces
+#if defined(__APPLE__) || defined(__linux)
+#include <dlfcn.h>
+#endif
+
 namespace dw::vulkan {
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+VulkanRTLPtr GetRuntimeLibs()
+{
+    VulkanRTLPtr vulkanLibrary;
+
+#if defined (_WIN32)
+    vulkanLibrary = LoadLibrary("vulkan-1.dll");
+#elif defined (__linux )
+    vulkanLibrary = dlopen("libvulkan.so.1", RTLD_NOW | RTLD_LOCAL);
+#elif defined (__APPLE__ )
+    vulkanLibrary = dlopen("libvulkan.1.dylib", RTLD_NOW | RTLD_LOCAL);
+#else
+    std::cerr << "The DireWolf renderer is not yet setup for Vulkan on this platform. Supported operating systems are Linux, Windows and Mac" << std::endl;
+    vulkanLibrary = nullptr;
+#endif
+
+    if (!vulkanLibrary) {
+        std::cerr << "Could not connect with a Vulkan Runtime library.\n";
+    }
+
+#if defined (DW_VERBOSE_LOG_VK)
+    std::cout << "\tSuccessfully connected with a Vulkan Runtime library.\n";
+#endif
+    return vulkanLibrary;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool InitProcAddress(VulkanRTLPtr vulkanRTL)
+{
+// TODO: move this #VK_NO_PROTOTYPES part elsewhere?
+#if defined (DW_VERBOSE_LOG_VK) && defined (VK_NO_PROTOTYPES)
+    std::cout << "\tVK_NO_PROTOTYPES is defined\n";
+#elif defined (DW_VERBOSE_LOG_VK) && !defined (VK_NO_PROTOTYPES)
+    std::cout << "\tWARNING: VK_NO_PROTOTYPES is NOT defined. This may become a potential performance issue.\n";
+#endif
+
+#if defined (_WIN32)
+  #define LoadFunction GetProcAddress
+#elif defined (__linux)
+  #define LoadFunction dlsym
+#elif defined (__APPLE__)
+  #define LoadFunction dlsym
+#endif
+
+#define EXPORTED_VULKAN_FUNCTION( name )                              \
+    name = (PFN_##name)LoadFunction( vulkanRTL, #name );              \
+    if ( name == nullptr ) {                                          \
+        std::cerr << "Could not load exported Vulkan function named: "\
+            #name << std::endl;                                       \
+        return false;                                                 \
+    }
+
+#include "listofvulkanfunctions.inl"
+
+    return true;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool InitGlobalLevelFunction()
+{
+#define GLOBAL_LEVEL_VULKAN_FUNCTION( name )                        \
+    name = (PFN_##name)vkGetInstanceProcAddr( nullptr, #name );     \
+    if ( name == nullptr ) {                                        \
+        std::cerr << "Could not load global-level function named: " \
+            #name << std::endl;                                     \
+        return false;                                               \
+    }
+
+#include "listofvulkanfunctions.inl"
+
+    return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool LoadInstanceLevelFunctions(const VkInstance& instance)
+{
+#define INSTANCE_LEVEL_VULKAN_FUNCTION( name )                              \
+    name = (PFN_##name)vkGetInstanceProcAddr( instance, #name );            \
+    if ( name == nullptr ) {                                                \
+        std::cerr << "Could not load instance-level Vulkan function named: "\
+            #name << std::endl;                                             \
+        return false;                                                       \
+    }                                                                       \
+
+#include "listofvulkanfunctions.inl"
+
+    return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool LoadInstanceLevelFunctionsFromExtensions(const VkInstance& instance, const std::vector<const char*>& enabledExtensions)
+{
+#define INSTANCE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION( name, extension )            \
+    for (const char* enabledExtension : enabledExtensions) {                        \
+        if (std::string(enabledExtension) == std::string(extension)) {              \
+            name = (PFN_##name)vkGetInstanceProcAddr(instance, #name);              \
+            if( name == nullptr ) {                                                 \
+                std::cerr << "Could not load instance-level Vulkan function named: "\
+                    #name << std::endl;                                             \
+                return false;                                                       \
+            }                                                                       \
+        }                                                                           \
+    }
+
+#include "listofvulkanfunctions.inl"
+
+    return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool IsExtensionSupportedImpl(const char* extension, const std::vector<VkExtensionProperties>& availableExtensions)
+{
+    for (const VkExtensionProperties& availableExtension : availableExtensions) {
+        if (strcmp(availableExtension.extensionName, extension) == 0) {
+            return true;
+        }
+    }
+
+    std::cerr << "WARNING: \"" << extension << "\" is not a valid extension!" << std::endl;
+
+    return false;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // overloaded operator<<
